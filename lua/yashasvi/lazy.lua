f33be735951312f7de008a264d87b7d01ff2d21e
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2024-01-11 11:36:39 +0000

    Adds trouble and treesitter-context

--- lua/yashasvi/lazy.lua
+++ lua/yashasvi/lazy.lua
@@ -34,6 +34,16 @@ local plugins = {
     {
     'ThePrimeagen/harpoon',
     dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    'nvim-treesitter/nvim-treesitter-context',
+    {
+    "folke/trouble.nvim",
+     dependencies = { "nvim-tree/nvim-web-devicons" },
+      opts = {
+            -- your configuration comes here
+            --   -- or leave it empty to use the default settings
+            --     -- refer to the configuration section below
+             },
     }
 }
 
