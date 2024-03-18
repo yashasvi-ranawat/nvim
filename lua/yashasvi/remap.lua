392f36edba5b776a1c92b11752b0f5d20ab06f1d
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2024-03-18 20:08:30 +0200

    Adds a command to run black

--- lua/yashasvi/remap.lua
+++ lua/yashasvi/remap.lua
@@ -40,3 +40,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- make executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- run black
+vim.keymap.set("n", "<leader>b", "<cmd>!black %<CR>", { silent = true })
