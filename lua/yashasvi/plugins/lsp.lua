e8ba3b917e20bad2522f85b19af6fd5eaee01789
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2024-07-15 09:54:10 +0000

    Remap <CR> to <C-y> for cmp to reduce <CR> interference

--- lua/yashasvi/plugins/lsp.lua
+++ lua/yashasvi/plugins/lsp.lua
@@ -74,7 +74,7 @@ return {
         local cmp_mappings = lsp.defaults.cmp_mappings({
             ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
             ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
             ["<C-Space>"] = cmp.mapping.complete(),
         })
 
