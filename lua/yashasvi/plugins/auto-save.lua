aff0cf844d41267450102938782467346cade759
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2024-07-31 09:14:56 +0000

    No autosave in oil fugitive gitcommit

--- lua/yashasvi/plugins/auto-save.lua
+++ lua/yashasvi/plugins/auto-save.lua
@@ -4,6 +4,17 @@ return {
         require("auto-save").setup {
             -- your config goes here
             -- or just leave it empty :)
+            condition = function(buf)
+                local fn = vim.fn
+                local utils = require("auto-save.utils.data")
+
+                if
+                    fn.getbufvar(buf, "&modifiable") == 1 and
+                    utils.not_in(fn.getbufvar(buf, "&filetype"), {"oil", "fugitive", "TelescopePrompt", "gitcommit"}) then
+                    return true -- met condition(s), can save
+                end
+                return false -- can't save
+            end,
         }
     end,
 }
