50475800c3fc9c85a60bef21be22fcbb400b071c
Yashasvi S. Ranawat <yashasvi.ranawat@gmail.com>
2025-02-14 01:26:04 +0200

    Priority load colorscheme

--- lua/yashasvi/plugins/colorscheme.lua
+++ lua/yashasvi/plugins/colorscheme.lua
@@ -1,11 +1,13 @@
 return {
-  'ellisonleao/gruvbox.nvim',
-  config = function()
-    require('gruvbox').setup({
-        contrast = 'soft',
-    })
-    vim.o.termguicolors = true
-    vim.o.background = 'light'
-    vim.cmd.colorscheme('gruvbox')
-  end,
+    'ellisonleao/gruvbox.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('gruvbox').setup({
+            contrast = 'soft',
+        })
+        vim.o.termguicolors = true
+        vim.o.background = 'light'
+        vim.cmd.colorscheme('gruvbox')
+    end,
 }
